a7f138ec16d350d17ad243a4294a64c193a016d9
Geo <realcoldf@gmail.com>
2024-01-27 15:02:50 +0100

    Updated docs

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,13 +2,48 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>Geo-VM</span>,
+
+  /* Announcement */
+  banner: {
+    key: '1.0',
+    text: (
+      <a href="#">
+        ✨ New documentation design.
+      </a>
+    )
+  },
+
+  /* Display the name */
+  useNextSeoProps() {
+    return {
+      titleTemplate: '%s - Geo-VM'
+    }
+  },
+
+  /* Embed part */
+  head: (
+    <>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta property="og:title" content="Geo-VM Docs" />
+    </>
+  ),
+
+  /* Logo */
+  logo: (
+    <> <img src="https://www.geo-vm.net/_next/image?url=%2Fassets%2Fimg%2FGeoVM.png&w=64&q=75" alt="Geo-VM" style={{ width: '50px', height: 'auto' }} /> </>
+  ),
+
+  /* GitHub button */
   project: {
     link: 'https://github.com/Geo-VM/Docs',
   },
+
+  /* Discord button */
   chat: {
     link: 'https://geo-vm.net/discord',
   },
+
+  /* GitHub stuff */
   docsRepositoryBase: 'https://github.com/Geo-VM/Docs',
   footer: {
     text: 'Geo-VM Documentation.',
